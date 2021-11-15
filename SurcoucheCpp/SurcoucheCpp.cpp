ae87d01dcc583a83d2ea5afb38c9eb1fd1f8c327
Thomas Hingray <GGTcommunity338@gmail.com>
2021-11-16 00:58:34 +0100

    test

--- SurcoucheCpp/SurcoucheCpp.cpp
+++ SurcoucheCpp/SurcoucheCpp.cpp
@@ -5,14 +5,14 @@
 int main() 
 {
 
-    std::string s("This is one sentence. This is a second sentence.");
-    std::regex re("(\\b(\\w+?)[,:;]?\\s?)+[?.!]");
+    std::string s("This is one sentence.");
+    const std::regex re("(\\b(\\w+?)[,:;]?\\s?)+[?.!]+");
 
     if (std::regex_match(s, re)) 
     {
         for (std::smatch m; std::regex_search(s, m, re); s = m.suffix())
         {
-            std::cout << m[1] << "\t\t" << m[2] << "\t\t" << m[3] << std::endl;
+            std::cout << m[0] << " " << m[1] << " " << m[2] << " " << m[3];
         }
     }
 
