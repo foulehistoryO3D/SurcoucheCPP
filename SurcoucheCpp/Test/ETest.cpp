0d73af975539bc2b144d89e5307029f312822178
foulehistory <GGTcommunity338@gmail.com>
2022-01-03 12:56:50 +0100

    Add Enum Test

---
+++ SurcoucheCpp/Test/ETest.cpp
@@ -0,0 +1,15 @@
+﻿#include "ETest.h"
+
+const ETest ETest::Thomas("Thomas", 22);
+const ETest ETest::Jean("Jean", 23);
+
+ETest::ETest(System::String name, System::Int age)
+{
+    this->name = name;
+    this->age = age;
+}
+
+System::String ETest::ToString() const
+{
+    return string("name: ") + name + ", age: " + age;
+}
