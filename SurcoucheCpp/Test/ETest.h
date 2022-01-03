b9d251134b31e865175275a7d444733d9ca34caa
foulehistory <GGTcommunity338@gmail.com>
2022-01-03 16:59:44 +0100

    Create ETest.h

---
+++ SurcoucheCpp/Test/ETest.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "../System/PrimaryType/Enum/Enum.h"
+#include "../System/PrimaryType/Integer/Integer.h"
+
+class ETest : public System::Enum<ETest>
+{
+private:
+    System::String name = System::String::Empty();
+    System::Int age = 0;
+public:
+    static const ETest Thomas;
+    static const ETest Jean;
+    ETest()=default;
+    ETest(System::String name, System::Int age);
+
+    System::String Name()const { return name;}
+    System::Int Age()const { return age; }
+
+    System::String ToString() const override;
+    
+};
