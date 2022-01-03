0460ce9f2a04c17b92f6ae8bed774d4664d0ffcc
foulehistory <GGTcommunity338@gmail.com>
2022-01-03 12:54:50 +0100

    Update Enum

--- SurcoucheCpp/SurcoucheCpp.cpp
+++ SurcoucheCpp/SurcoucheCpp.cpp
@@ -1,9 +1,10 @@
 #include "System/IncludeSystem.h"
 #include "System/Window/Window.h"
+#include "Test/ETest.h"
 
 int main()
 {
-    Window _window = Window("Test", 500,500, false);
-    
+    // Window _window = Window("Test", 500,500, false);
+    Console::WriteLine(ETest::Jean);
     return 0;
 }
--- SurcoucheCpp/System/PrimaryType/Enum/Enum.h
+++ SurcoucheCpp/System/PrimaryType/Enum/Enum.h
@@ -2,44 +2,63 @@
 #include <map>
 #include <string>
 
+#include "../../Console/Console.h"
+#include "../../Exception/OutOfRange/OutOfRange.h"
 #include "../../Object/Object.h"
+#include "../Boolean/Boolean.h"
 
 namespace System
 {
     class Integer;
-    
+
+    template<typename T>
     struct EnumPair : Object
     {
         int mValue;
-        const char* mName;
+        T mItem;
 
-        EnumPair(int _value, const char* _name)
+        EnumPair(int _value, T _item)
         {
             mValue = _value;
-            mName = _name;
+            mItem = _item;
         }
 
         int Value()const { return mValue; }
-        const char* Name()const { return mName; }
+        T Item()const { return mItem; }
         
     };
-    
+
+    template<typename T>
     class Enum : public Object
     {
         DECLARE_CLASS_INFO(Object)
 #pragma region f/p
     private:
-        std::map<const char*, EnumPair> mContainers = std::map<const char*, EnumPair>();
+        std::map<const char*, EnumPair<T>> mContainers = std::map<const char*, EnumPair<T>>();
 #pragma endregion f/p
 #pragma region constructor
     public:
         Enum()=default;
-        Enum(std::initializer_list<EnumPair> _pair);
+        Enum(std::initializer_list<EnumPair<T>> _pair);
 #pragma endregion constructor
 #pragma region operator
     public:
-        String operator[](const int32 _index);
-        Boolean TryParse(const char* _name, Boolean _ignoreCase, Integer& _enum);
+        T operator[](const int32 _index);
 #pragma endregion operator
     };
+
+    template <typename T>
+    Enum<T>::Enum(std::initializer_list<EnumPair<T>> _pair)
+    {
+        for (EnumPair<T>& _item : _pair)
+            mContainers.insert(std::pair<int, EnumPair<T>>(_item.Value(), _item.Item()));
+    }
+
+    template <typename T>
+    T Enum<T>::operator[](const int32 _index)
+    {
+        if (_index < 0 || _index > mContainers.size())
+            throw OutOfRange("[Enum] out of range !");
+        return mContainers[_index];
+    }
 }
